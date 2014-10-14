3587435b0342210f3310238b59afeb721c291231
masuhajime <masuhajime@masuhajime.local>
2014-10-15 04:48:09 +0900

    (TwT)itter

--- amoamo/platform/ios/service/Twitter.mm
+++ amoamo/platform/ios/service/Twitter.mm
@@ -3,12 +3,36 @@
 
 #include "Twitter.h"
 
+#import "AppController.h"
+#import <Social/Social.h>
+
 namespace amoamo {
     
-void Twitter::tweet(const char* tweet) {
-    CCLOG("void Twitter::tweet(const char* tweet) {");
-}
+void Twitter::tweet(const char* tweet)
+{
+    CCLOG("Tweeting:%s", tweet);
+    NSString* tweet_string = [NSString stringWithUTF8String:tweet];
     
+    if ([SLComposeViewController isAvailableForServiceType:SLServiceTypeTwitter]) {
+        SLComposeViewController *tweetSheet = [SLComposeViewController composeViewControllerForServiceType:SLServiceTypeTwitter];
+        [tweetSheet setInitialText:tweet_string];
+        [tweetSheet setCompletionHandler:^(SLComposeViewControllerResult result) {
+            if (result == SLComposeViewControllerResultDone) {
+                //投稿成功時の処理
+            }
+        }];
+        UIViewController* rootController = [UIApplication sharedApplication].keyWindow.rootViewController;
+        [rootController presentViewController:tweetSheet animated:YES completion:nil];
+    }
+    else
+    {
+        tweet_string = [NSString stringWithFormat:@"http://twitter.com/home?status=%@", tweet_string];
+        tweet_string = [tweet_string stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+        NSURL *url = [NSURL URLWithString:tweet_string];
+        [[UIApplication sharedApplication] openURL:url];
+    }
 }
+
+} /* namespace amoamo */
 
 #endif /* AMOAMO_PLATFORM_IS_IOS */
