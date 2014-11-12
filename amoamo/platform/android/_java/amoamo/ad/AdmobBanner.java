a7f3b5a3a0477b2945ab19fc3f98a5bc009036da
masuhajime <masuhajime@masuhajime.local>
2014-11-12 12:08:20 +0900

    java ad

--- amoamo/platform/android/_java/amoamo/ad/AdmobBanner.java
+++ amoamo/platform/android/_java/amoamo/ad/AdmobBanner.java
@@ -3,37 +3,52 @@ package amoamo.ad;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
+import org.cocos2dx.cpp.AppActivity;
 import org.cocos2dx.lib.Cocos2dxActivity;
 
-import android.annotation.TargetApi;
+import android.annotation.SuppressLint;
 import android.graphics.Color;
 import android.graphics.Point;
-import android.os.Build;
 import android.os.Bundle;
 import android.view.Display;
-import android.view.View;
-import android.view.WindowManager;
+import android.view.Gravity;
+import android.widget.FrameLayout;
+import android.widget.FrameLayout.LayoutParams;
 import android.widget.LinearLayout;
 
 import com.google.android.gms.ads.AdSize;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 
-public abstract class AdmobBanner
-{
-    private static final Cocos2dxActivity activity = (Cocos2dxActivity)Cocos2dxActivity.getContext();
+public abstract class AdmobBanner extends Cocos2dxActivity {
     
-    public static void init() {
-        adView = new AdView(this);
+    private static AdView adView = null;
+    
+    private final static int lp = LinearLayout.LayoutParams.WRAP_CONTENT; 
+    
+    public static void init(AppActivity appActivity, String adUnitId) {
+    	if (adView != null) {
+    		return;
+    	}
+        adView = new AdView(appActivity);
         adView.setAdSize(AdSize.BANNER);
-        adView.setAdUnitId("ca-app-pub-から始まる広告ユニットIDをここに");
+        adView.setAdUnitId(adUnitId);
         
-        FrameLayout.LayoutParams adParams = new FrameLayout.LayoutParams(lp,lp);
+        FrameLayout.LayoutParams adParams = new FrameLayout.LayoutParams(lp, lp);
         adParams.gravity = (Gravity.BOTTOM|Gravity.CENTER);
-        addContentView(adView, adParams);
         
-        AdRequest adRequest = new AdRequest.Builder().build();
+        AdRequest adRequest = new AdRequest.Builder().addTestDevice(AdRequest.DEVICE_ID_EMULATOR).build();
         
         adView.loadAd(adRequest);
+        
+        //addContentView(adView, adParams);
+    }
+    
+    public static void getAdView() {
+    	
+    }
+    
+    public static void getAdParams() {
+    	
     }
 }
