f4254220b43fc29bc4b8efcb2a5927304e1cc606
masuhajime <masuhajime@gmail.com>
2015-05-25 03:25:29 -0400

    add amoamo::common::string

---
+++ amoamo/common/String.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <string>
+
+namespace amoamo {
+namespace common {
+
+class String {
+public:
+    static std::string urlencode(std::string string);
+};
+
+}// namespace common
+}// namespace amoamo
